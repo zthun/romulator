d93d46431e181e8e310d58e217d6eea60cf733dd
Anthony Bonta <zthun.coding@gmail.com>
2025-01-04 10:46:38 -0600

    feat: romulator ui favicon

--- packages/romulator-web/src/app/app-title.tsx
+++ packages/romulator-web/src/app/app-title.tsx
@@ -4,7 +4,7 @@ export function ZRomulatorTitle() {
   return (
     <div className="ZRomulatorTitle-root">
       <ZH1 compact>Romulator</ZH1>
-      <ZCaption compact>Organize your ROMs</ZCaption>
+      <ZCaption compact>Organize your Games</ZCaption>
     </div>
   );
 }
