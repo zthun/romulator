468c7b5d63563c27ed233ecb370088ba39b3df68
Anthony Bonta <zthun.coding@gmail.com>
2025-01-04 13:49:18 -0600

    feat: systems service retrieves the current systems that are discovered

---
+++ packages/romulator-web/src/systems/systems-service.mts
@@ -0,0 +1,52 @@
+import { IZDataRequest, IZDataSource } from "@zthun/helpful-query";
+import { IZRomulatorSystem } from "@zthun/romulator";
+import { IZHttpService, ZHttpService } from "@zthun/webigail-http";
+import {
+  IZRestfulGet,
+  IZRestfulService,
+  ZRestfulService,
+} from "@zthun/webigail-rest";
+import { ZUrlBuilder } from "@zthun/webigail-url";
+import { createContext, useContext } from "react";
+
+export interface IZRomulatorSystemsService
+  extends IZRestfulGet<IZRomulatorSystem>,
+    IZDataSource<IZRomulatorSystem> {}
+
+export class ZRomulatorSystemsService implements IZRomulatorSystemsService {
+  private _rest: IZRestfulService<IZRomulatorSystem>;
+
+  public static endpoint() {
+    return new ZUrlBuilder().api(location).append("systems").build();
+  }
+
+  public constructor(_http: IZHttpService) {
+    this._rest = new ZRestfulService(
+      _http,
+      ZRomulatorSystemsService.endpoint(),
+    );
+  }
+
+  public async retrieve(request: IZDataRequest): Promise<IZRomulatorSystem[]> {
+    return this._rest.retrieve(request);
+  }
+
+  public async count(request: IZDataRequest): Promise<number> {
+    return this._rest.count(request);
+  }
+
+  public async get(id: string): Promise<IZRomulatorSystem> {
+    return this._rest.get(id);
+  }
+}
+
+export function createDefaultSystemsService(): IZRomulatorSystemsService {
+  return new ZRomulatorSystemsService(new ZHttpService());
+}
+
+export const ZRomulatorSystemsServiceContext = createContext(
+  createDefaultSystemsService(),
+);
+
+export const useSystemsService = () =>
+  useContext(ZRomulatorSystemsServiceContext);
---
+++ packages/romulator-web/src/systems/systems-service.spec.ts
@@ -0,0 +1,137 @@
+import { IZCircusSetup } from "@zthun/cirque";
+import { IZCircusReactHook, ZCircusSetupHook } from "@zthun/cirque-du-react";
+import { ZDataRequestBuilder, ZPageBuilder } from "@zthun/helpful-query";
+import { IZRomulatorSystem, ZRomulatorSystemBuilder } from "@zthun/romulator";
+import {
+  ZHttpMethod,
+  ZHttpResultBuilder,
+  ZHttpServiceMock,
+} from "@zthun/webigail-http";
+import { ZRestfulUrlBuilder } from "@zthun/webigail-rest";
+import { ZUrlBuilder } from "@zthun/webigail-url";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import {
+  IZRomulatorSystemsService,
+  useSystemsService,
+  ZRomulatorSystemsService,
+} from "./systems-service.mjs";
+
+describe("SystemsService", () => {
+  let _http: ZHttpServiceMock;
+
+  const nes = new ZRomulatorSystemBuilder().nes().build();
+  const snes = new ZRomulatorSystemBuilder().snes().build();
+  const systems = [nes, snes];
+
+  beforeEach(async () => {
+    _http = new ZHttpServiceMock();
+
+    _http.set(
+      ZRomulatorSystemsService.endpoint(),
+      ZHttpMethod.Get,
+      new ZHttpResultBuilder(
+        new ZPageBuilder<IZRomulatorSystem>().all(systems).build(),
+      ).build(),
+    );
+
+    _http.set(
+      new ZRestfulUrlBuilder(ZRomulatorSystemsService.endpoint())
+        .count()
+        .build(),
+      ZHttpMethod.Get,
+      new ZHttpResultBuilder(
+        new ZPageBuilder<IZRomulatorSystem>()
+          .data([nes])
+          .count(systems.length)
+          .build(),
+      ).build(),
+    );
+
+    _http.set(
+      new ZUrlBuilder()
+        .parse(ZRomulatorSystemsService.endpoint())
+        .append(nes.id)
+        .build(),
+      ZHttpMethod.Get,
+      new ZHttpResultBuilder(nes).build(),
+    );
+
+    _http.set(
+      new ZUrlBuilder()
+        .parse(ZRomulatorSystemsService.endpoint())
+        .append(snes.id)
+        .build(),
+      ZHttpMethod.Get,
+      new ZHttpResultBuilder(snes).build(),
+    );
+  });
+
+  const createTestTarget = () => new ZRomulatorSystemsService(_http);
+
+  describe("Read", () => {
+    it("should retrieve a list of systems", async () => {
+      // Arrange.
+      const target = createTestTarget();
+      const request = new ZDataRequestBuilder().build();
+
+      // Act.
+      const actual = await target.retrieve(request);
+
+      // Assert.
+      expect(actual).toEqual(systems);
+    });
+
+    it("should retrieve a single system by id", async () => {
+      // Arrange.
+      const target = createTestTarget();
+
+      // Act.
+      const _nes = await target.get(nes.id);
+      const _snes = await target.get(snes.id);
+
+      // Assert.
+      expect(_nes).toEqual(nes);
+      expect(_snes).toEqual(snes);
+    });
+  });
+
+  describe("Count", () => {
+    it("should count the number of systems", async () => {
+      // Arrange.
+      const target = createTestTarget();
+
+      // Act.
+      const actual = await target.count(new ZDataRequestBuilder().build());
+
+      // Assert.
+      expect(actual).toEqual(systems.length);
+    });
+  });
+});
+
+describe("useSystemsService", () => {
+  let _hook: IZCircusSetup<
+    IZCircusReactHook<IZRomulatorSystemsService, unknown>
+  >;
+
+  const createTestTarget = () => {
+    _hook = new ZCircusSetupHook(() => useSystemsService());
+
+    return _hook?.setup();
+  };
+
+  afterEach(async () => {
+    await _hook?.destroy?.call(_hook);
+  });
+
+  it("should have a default implementation", async () => {
+    // Arrange.
+    const target = await createTestTarget();
+
+    // Act.
+    const actual = await target.current();
+
+    // Assert.
+    expect(actual).toBeInstanceOf(ZRomulatorSystemsService);
+  });
+});
