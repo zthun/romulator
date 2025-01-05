e828edc49df7d894a177447bfe498f7dfd4ecfe8
Anthony Bonta <zthun.coding@gmail.com>
2025-01-04 23:44:07 -0600

    refactor: add media folder to config

--- packages/romulator/src/config/config.ts
+++ packages/romulator/src/config/config.ts
@@ -1,5 +1,6 @@
 export interface IZRomulatorConfig {
   games?: string;
+  media?: string;
 }
 
 export class ZRomulatorConfigBuilder {
@@ -11,6 +12,11 @@ export class ZRomulatorConfigBuilder {
 
   public games(path: string): this {
     this._config.games = path;
+    return this;
+  }
+
+  public media(path: string): this {
+    this._config.media = path;
     return this;
   }
 
