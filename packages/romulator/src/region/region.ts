13a5c3f8f97f5c59fb03ab7a957feabf68a70817
Anthony Bonta <zthun.coding@gmail.com>
2025-05-27 19:33:34 -0500

    refactor: add region builder

--- packages/romulator/src/region/region.ts
+++ packages/romulator/src/region/region.ts
@@ -50,8 +50,49 @@ export enum ZRomulatorRegion {
   /**
    * World.
    *
-   * This is the default value.  Any
-   * optional should fall back to this value.
+   * This region is used for games that are released
+   * globally without specific regional variations.
    */
   World = "world",
+}
+
+/**
+ * Represents a mapping of regions to data.
+ */
+export type ZRomulatorRegionMap<T> = Partial<Record<ZRomulatorRegion, T>>;
+
+/**
+ * A builder for creating a region map for values.
+ */
+export class ZRomulatorRegionMapBuilder<T> {
+  private _map: ZRomulatorRegionMap<T> = {};
+
+  /**
+   * Sets the value for a specific region.
+   * @param region
+   *        The region to set the value for.
+   * @param value
+   *        The value to set.
+   * @returns
+   *        This instance.
+   */
+  public set(region: ZRomulatorRegion, value: T): this {
+    this._map[region] = value;
+    return this;
+  }
+
+  public aus = this.set.bind(this, ZRomulatorRegion.Australia);
+  public br = this.set.bind(this, ZRomulatorRegion.Brazil);
+  public chl = this.set.bind(this, ZRomulatorRegion.Chile);
+  public eu = this.set.bind(this, ZRomulatorRegion.Europe);
+  public irl = this.set.bind(this, ZRomulatorRegion.Ireland);
+  public jp = this.set.bind(this, ZRomulatorRegion.Japan);
+  public na = this.set.bind(this, ZRomulatorRegion.NorthAmerica);
+  public ru = this.set.bind(this, ZRomulatorRegion.Russia);
+  public uk = this.set.bind(this, ZRomulatorRegion.UnitedKingdom);
+  public world = this.set.bind(this, ZRomulatorRegion.World);
+
+  public build(): ZRomulatorRegionMap<T> {
+    return structuredClone(this._map);
+  }
 }
