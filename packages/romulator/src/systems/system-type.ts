9fcf9f25a16b46c07cec4b6acf11767d20f68c87
Anthony Bonta <zthun.coding@gmail.com>
2025-05-28 08:09:41 -0500

    refactor: add system types

--- packages/romulator/src/systems/system-type.ts
+++ packages/romulator/src/systems/system-type.ts
@@ -1,3 +1,21 @@
+/**
+ * Describes a type of system.
+ */
 export enum ZRomulatorSystemType {
+  /**
+   * A console system.
+   */
   Console = "console",
+  /**
+   * A handheld system
+   */
+  Handheld = "handheld",
+  /**
+   * A cabinet system
+   */
+  Arcade = "arcade",
+  /**
+   * Known computer systems
+   */
+  Computer = "computer",
 }
