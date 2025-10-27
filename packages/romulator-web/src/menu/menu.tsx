4bc7154a392560eb0afe46888cbab1f879c4c419
Anthony Bonta <zthun.coding@gmail.com>
2025-10-27 11:06:22 -0500

    feat: game menu item sub heading is now Browse your library

--- packages/romulator-web/src/menu/menu.tsx
+++ packages/romulator-web/src/menu/menu.tsx
@@ -81,7 +81,7 @@ export function ZRomulatorMenu() {
             <ZContentTitle
               avatar={<ZIconFontAwesome name="gamepad" />}
               heading={<ZH3 compact>Games</ZH3>}
-              subHeading={<ZCaption>View all games</ZCaption>}
+              subHeading={<ZCaption>Browse your library</ZCaption>}
             />
           </ZListItem>
 
